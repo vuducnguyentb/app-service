c9feba2ff3c22d995eed9e3cc776cf26c607566f
Nguyên Vũ <vuducnguyentb@gmail.com>
2024-02-06 09:52:23 +0700

    update

--- resources/views/client/layouts/footer.blade.php
+++ resources/views/client/layouts/footer.blade.php
@@ -1,89 +1,90 @@
-<footer id="footer" class="footer-light">
-    <div class="footer-content">
+<footer class="footer footer-2">
+    <div class="container">
+        <hr class="mb-7">
+        <div class="row">
+            <div class="col-2xl-5col col-md-12">
+                <div class="widget widget-about">
+                    <img src="{{asset('assets/images/demos/demo-21/logo-footer.png')}}" class="footer-logo" alt="Footer Logo" width="100" height="25">
+                    <p>Praesent dapibus, neque id cursus ucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus. </p>
+
+                    <div class="widget-about-info">
+                        <div class="row">
+                            <div class="col-sm-6 col-md-4 phoneNum">
+                                <span class="widget-about-title">Got Question? Call us 24/7</span>
+                                <a href="tel:123456789">+0123 456 789</a>
+                            </div><!-- End .col-sm-6 -->
+                            <div class="col-sm-6 col-md-8 payment">
+                                <span class="widget-about-title">Payment Method</span>
+                                <figure class="footer-payments">
+                                    <img src="{{asset('assets/images/payments.png')}}" alt="Payment methods" width="272" height="20">
+                                </figure><!-- End .footer-payments -->
+                            </div><!-- End .col-sm-6 -->
+                        </div><!-- End .row -->
+                    </div><!-- End .widget-about-info -->
+                </div><!-- End .widget about-widget -->
+            </div><!-- End .col-sm-12 col-lg-3 -->
+
+            <div class="col-xl-5col col-sm-4 col-md-4">
+                <div class="widget">
+                    <h4 class="widget-title">Information</h4><!-- End .widget-title -->
+
+                    <ul class="widget-list">
+                        <li><a href="about.html">About Molla</a></li>
+                        <li><a href="#">How to shop on Molla</a></li>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="contact.html">Contact us</a></li>
+                        <li><a href="login.html">Log in</a></li>
+                    </ul><!-- End .widget-list -->
+                </div><!-- End .widget -->
+            </div><!-- End .col-sm-4 col-lg-3 -->
+
+            <div class="col-xl-5col col-sm-4 col-md-6=4">
+                <div class="widget">
+                    <h4 class="widget-title">Customer Service</h4><!-- End .widget-title -->
+
+                    <ul class="widget-list">
+                        <li><a href="#">Payment Methods</a></li>
+                        <li><a href="#">Money-back guarantee!</a></li>
+                        <li><a href="#">Returns</a></li>
+                        <li><a href="#">Shipping</a></li>
+                        <li><a href="#">Terms and conditions</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                    </ul><!-- End .widget-list -->
+                </div><!-- End .widget -->
+            </div><!-- End .col-sm-4 col-lg-3 -->
+
+            <div class="col-xl-5col col-sm-4 col-md-4">
+                <div class="widget">
+                    <h4 class="widget-title">My Account</h4><!-- End .widget-title -->
+
+                    <ul class="widget-list">
+                        <li><a href="#">Sign In</a></li>
+                        <li><a href="cart.html">View Cart</a></li>
+                        <li><a href="#">My Wishlist</a></li>
+                        <li><a href="#">Track My Order</a></li>
+                        <li><a href="#">Help</a></li>
+                    </ul><!-- End .widget-list -->
+                </div><!-- End .widget -->
+            </div><!-- End .col-sm-64 col-lg-3 -->
+        </div><!-- End .row -->
+    </div><!-- End .container -->
+
+    <div class="footer-bottom">
         <div class="container">
-            <div class="row">
-                <div class="col-md-4">
-                    <!-- Footer widget area 1 -->
-                    <div class="widget clearfix widget-contact-us" style="background-image: url('images/world-map-dark.png'); background-position: 50% 20px; background-repeat: no-repeat">
-                        <h4>Thông tin BATDANGOAI</h4>
-                        <ul class="list-icon">
-                            <li><i class="fa fa-map-marker"></i>237 Ngõ 141 Giáp Nhị - Thịnh Liệt - Hoàng Mai
-                                <br>(Cạnh đầu sông Sét)</li>
-                            <li><i class="fa fa-phone"></i> 0904.779.796 - 0967.277.142</li>
-                            <li><i class="fa fa-envelope"></i> <a href="mailto:first.last@example.com">batdangoai@gmail.com</a>
-                            </li>
-                            <li>
-                                <br><i class="fa fa-clock-o"></i>Thứ 2- Chủ Nhật: <strong>08:00 - 22:00</strong>
-{{--                                <br>Saturday, Sunday: <strong>Closed</strong>--}}
-                            </li>
-                        </ul>
-                        <!-- Social icons -->
-                        <div class="social-icons social-icons-border float-left m-t-20">
-                            <ul>
-                                <li class="social-rss"><a href="#"><i class="fa fa-rss"></i></a></li>
-                                <li class="social-facebook"><a href="#"><i class="fa fa-facebook"></i></a></li>
-                                <li class="social-twitter"><a href="#"><i class="fa fa-twitter"></i></a></li>
-                                <li class="social-vimeo"><a href="#"><i class="fa fa-vimeo"></i></a></li>
-                                <li class="social-youtube"><a href="#"><i class="fa fa-youtube"></i></a></li>
-                                <li class="social-pinterest"><a href="#"><i class="fa fa-pinterest"></i></a></li>
-                                <li class="social-gplus"><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                            </ul>
-                        </div>
-                        <!-- end: Social icons -->
-                    </div>
-                    <!-- end: Footer widget area 1 -->
-                </div>
-                <div class="col-md-2">
-                    <!-- Footer widget area 2 -->
-                    <div class="widget">
-                        <h4>THÔNG TIN CHÍNH SÁCH</h4>
-                        <ul class="list-icon list-icon-arrow">
-                            <li><a href="/gioi-thieu">Giới thiệu</a></li>
-                            <li><a href="/quy-dinh">Quy định hình thức thanh toán</a></li>
-                            <li><a href="/chinh-sach">Chính sách vận chuyển,giao nhận</a></li>
-                            <li><a href="/">Trang chủ</a></li>
-                            <li><a href="{{route('news')}}">Tin tức</a></li>
-                        </ul>
-                    </div>
-                    <!-- end: Footer widget area 2 -->
-                </div>
-                <div class="col-md-3">
-                    <!-- Footer widget area 3 -->
-                    <div class="widget">
-                        <h4>Tin tức mới nhât.</h4>
-                        <div class="post-thumbnail-list">
-                            @foreach($postNewFooters as $key=>$item)
-                            <div class="post-thumbnail-entry">
-                                <div class="post-thumbnail-content">
-                                    <a href="#">{{$item['title']}}</a>
-                                    <span class="post-date"><i class="fa fa-clock-o"></i> {{$item['created_at']}}</span>
-                                    @foreach($item['categories'] as $key=>$value)
-                                    <span class="post-category"><i class="fa fa-tag"></i>{{$value['name']}}</span>
-                                    @endforeach
-                                </div>
-                            </div>
-                            @endforeach
-                        </div>
-                    </div>
-                    <!-- end: Footer widget area 3 -->
-                </div>
-                <div class="col-md-3">
-                    <!-- Footer widget area 4 -->
-                    <div class="widget widget-tweeter" data-username="ardianmusliu" data-limit="2">
-                        <h4>Map</h4>
-                        <div id="map-batdao">
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1862.7433728123797!2d105.85015316910489!3d20.97311707914733!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ac442d30e95b%3A0x337ca3a2d41ba73!2zMjc1IFAuIEdpw6FwIE5o4buLLCBUaOG7i25oIExp4buHdCwgSGFpIELDoCBUcsawbmcsIEjDoCBO4buZaSwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1705314124348!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                        </div>
-                    </div>
-                    <!-- end: Footer widget area 4 -->
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="copyright-content">
-        <div class="container">
-            <div class="copyright-text text-center">&copy; 2024 Bạt dã ngoại . All Rights Reserved.
-            </div>
-        </div>
-    </div>
-</footer>
+            <p class="footer-copyright">Copyright © 2019 Molla Store. All Rights Reserved.</p><!-- End .footer-copyright -->
+            <ul class="footer-menu">
+                <li><a href="#">Terms Of Use</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+            </ul><!-- End .footer-menu -->
+
+            <div class="social-icons social-icons-color">
+                <span class="social-label">Social Media</span>
+                <a href="#" class="social-icon social-facebook" title="Facebook" target="_blank"><i class="icon-facebook-f"></i></a>
+                <a href="#" class="social-icon social-twitter" title="Twitter" target="_blank"><i class="icon-twitter"></i></a>
+                <a href="#" class="social-icon social-instagram" title="Instagram" target="_blank"><i class="icon-instagram"></i></a>
+                <a href="#" class="social-icon social-youtube" title="Youtube" target="_blank"><i class="icon-youtube"></i></a>
+                <a href="#" class="social-icon social-pinterest" title="Pinterest" target="_blank"><i class="icon-pinterest"></i></a>
+            </div><!-- End .soial-icons -->
+        </div><!-- End .container -->
+    </div><!-- End .footer-bottom -->
+</footer><!-- End .footer -->
--- resources/views/client/layouts/main.blade.php
+++ resources/views/client/layouts/main.blade.php
@@ -1,51 +1,207 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
+<!-- molla/index-21.html  22 Nov 2019 10:01:31 GMT -->
 <head>
-    @yield('seo_support')
-    <link rel="shortcut icon" type="image/png" href="{{asset('assets/images/favicon.png')}}"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <meta name="author" content="BatDaNgoai" />
-    <!-- Document title -->
-    <!-- Stylesheets & Fonts -->
-    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,800,700,600|Montserrat:400,500,600,700|Raleway:100,300,600,700,800" rel="stylesheet" type="text/css" />
-    <link href="{{asset('assets/css/plugins.css')}}" rel="stylesheet">
-    <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
-    <link href="{{asset('assets/css/responsive.css')}}" rel="stylesheet"> </head>
+    <title>Molla - Bootstrap eCommerce Template</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <meta name="keywords" content="HTML5 Template">
+    <meta name="description" content="Molla - Bootstrap eCommerce Template">
+    <meta name="author" content="p-themes">
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('assets/images/icons/apple-touch-icon.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('assets/images/icons/favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('assets/images/icons/favicon-16x16.png')}}">
+    <link rel="manifest" href="{{asset('assets/images/icons/site.html')}}">
+    <link rel="mask-icon" href="{{asset('assets/images/icons/safari-pinned-tab.svg')}}" color="#666666">
+    <link rel="shortcut icon" href="{{asset('assets/images/icons/favicon.ico')}}">
+    <meta name="apple-mobile-web-app-title" content="Molla">
+    <meta name="application-name" content="Molla">
+    <meta name="msapplication-TileColor" content="#cc9966">
+    <meta name="msapplication-config" content="{{asset('assets/images/icons/browserconfig.xml')}}">
+    <meta name="theme-color" content="#ffffff">
+    <!-- Plugins CSS File -->
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/plugins/owl-carousel/owl.carousel.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/plugins/magnific-popup/magnific-popup.css')}}">
+    <!-- Main CSS File -->
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/skins/skin-demo-21.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/demos/demo-21.css')}}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;200;300;400;600&display=swap" rel="stylesheet">    <style>
+        body{
+            font-family: 'Roboto Slab', serif !important;
+            font-size: 14px;
+        }
+    </style>
+</head>
 <body>
-
-
-<!-- Wrapper -->
-<div id="wrapper">
-
-    <!-- TOPBAR -->
-{{--@include('client.layouts.topbar')--}}
-<!-- end: TOPBAR -->
-
+<div class="page-wrapper">
     <!-- Header -->
 @include('client.layouts.header')
 <!-- end: Header -->
-
 @yield('content')
 
     <!-- Footer -->
 @include('client.layouts.footer')
 <!-- end: Footer -->
 
-</div>
-<!-- end: Wrapper -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-@include('client.layouts.contact')
-<!-- Go to top button -->
-<a id="goToTop"><i class="fa fa-angle-up top-icon"></i><i class="fa fa-angle-up"></i></a>
-<!--Plugins-->
-<script src="{{asset('assets/js/jquery.js')}}"></script>
-<script src="{{asset('assets/js/plugins.js')}}"></script>
+    <button id="scroll-top" title="Back to Top"><i class="icon-arrow-up"></i></button>
 
-<!--Template functions-->
-<script src="{{asset('assets/js/functions.js')}}"></script>
+
+    <div class="mobile-menu-overlay"></div><!-- End .mobil-menu-overlay -->
+
+    @include('client.layouts.header_mobile')
+
+
+</div>
+
+<!-- Sign in / Register Modal -->
+<div class="modal fade" id="signin-modal" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-body">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true"><i class="icon-close"></i></span>
+                </button>
+
+                <div class="form-box">
+                    <div class="form-tab">
+                        <ul class="nav nav-pills nav-fill" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link active" id="signin-tab" data-toggle="tab" href="#signin" role="tab"
+                                   aria-controls="signin" aria-selected="true">Sign In</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="register-tab" data-toggle="tab" href="#register" role="tab"
+                                   aria-controls="register" aria-selected="false">Register</a>
+                            </li>
+                        </ul>
+                        <div class="tab-content" id="tab-content-5">
+                            <div class="tab-pane fade show active" id="signin" role="tabpanel"
+                                 aria-labelledby="signin-tab">
+                                <form action="#">
+                                    <div class="form-group">
+                                        <label for="singin-email">Username or email address *</label>
+                                        <input type="text" class="form-control" id="singin-email" name="singin-email"
+                                               required>
+                                    </div><!-- End .form-group -->
+
+                                    <div class="form-group">
+                                        <label for="singin-password">Password *</label>
+                                        <input type="password" class="form-control" id="singin-password"
+                                               name="singin-password" required>
+                                    </div><!-- End .form-group -->
+
+                                    <div class="form-footer">
+                                        <button type="submit" class="btn btn-outline-primary-2">
+                                            <span>LOG IN</span>
+                                            <i class="icon-long-arrow-right"></i>
+                                        </button>
+
+                                        <div class="custom-control custom-checkbox">
+                                            <input type="checkbox" class="custom-control-input" id="signin-remember">
+                                            <label class="custom-control-label" for="signin-remember">Remember
+                                                Me</label>
+                                        </div><!-- End .custom-checkbox -->
+
+                                        <a href="#" class="forgot-link">Forgot Your Password?</a>
+                                    </div><!-- End .form-footer -->
+                                </form>
+                                <div class="form-choice">
+                                    <p class="text-center">or sign in with</p>
+                                    <div class="row">
+                                        <div class="col-sm-6">
+                                            <a href="#" class="btn btn-login btn-g">
+                                                <i class="icon-google"></i>
+                                                Login With Google
+                                            </a>
+                                        </div><!-- End .col-6 -->
+                                        <div class="col-sm-6">
+                                            <a href="#" class="btn btn-login btn-f">
+                                                <i class="icon-facebook-f"></i>
+                                                Login With Facebook
+                                            </a>
+                                        </div><!-- End .col-6 -->
+                                    </div><!-- End .row -->
+                                </div><!-- End .form-choice -->
+                            </div><!-- .End .tab-pane -->
+                            <div class="tab-pane fade" id="register" role="tabpanel" aria-labelledby="register-tab">
+                                <form action="#">
+                                    <div class="form-group">
+                                        <label for="register-email">Your email address *</label>
+                                        <input type="email" class="form-control" id="register-email"
+                                               name="register-email" required>
+                                    </div><!-- End .form-group -->
+
+                                    <div class="form-group">
+                                        <label for="register-password">Password *</label>
+                                        <input type="password" class="form-control" id="register-password"
+                                               name="register-password" required>
+                                    </div><!-- End .form-group -->
+
+                                    <div class="form-footer">
+                                        <button type="submit" class="btn btn-outline-primary-2">
+                                            <span>SIGN UP</span>
+                                            <i class="icon-long-arrow-right"></i>
+                                        </button>
+
+                                        <div class="custom-control custom-checkbox">
+                                            <input type="checkbox" class="custom-control-input" id="register-policy"
+                                                   required>
+                                            <label class="custom-control-label" for="register-policy">I agree to the <a
+                                                    href="#">privacy policy</a> *</label>
+                                        </div><!-- End .custom-checkbox -->
+                                    </div><!-- End .form-footer -->
+                                </form>
+                                <div class="form-choice">
+                                    <p class="text-center">or sign in with</p>
+                                    <div class="row">
+                                        <div class="col-sm-6">
+                                            <a href="#" class="btn btn-login btn-g">
+                                                <i class="icon-google"></i>
+                                                Login With Google
+                                            </a>
+                                        </div><!-- End .col-6 -->
+                                        <div class="col-sm-6">
+                                            <a href="#" class="btn btn-login  btn-f">
+                                                <i class="icon-facebook-f"></i>
+                                                Login With Facebook
+                                            </a>
+                                        </div><!-- End .col-6 -->
+                                    </div><!-- End .row -->
+                                </div><!-- End .form-choice -->
+                            </div><!-- .End .tab-pane -->
+                        </div><!-- End .tab-content -->
+                    </div><!-- End .form-tab -->
+                </div><!-- End .form-box -->
+            </div><!-- End .modal-body -->
+        </div><!-- End .modal-content -->
+    </div><!-- End .modal-dialog -->
+</div><!-- End .modal -->
+
+{{--@include('client.layouts.subscribe')--}}
+
+
+<!-- Plugins JS File -->
+<script src="{{asset('assets/js/jquery.min.js')}}"></script>
+<script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('assets/js/jquery.hoverIntent.min.js')}}"></script>
+<script src="{{asset('assets/js/jquery.waypoints.min.js')}}"></script>
+<script src="{{asset('assets/js/superfish.min.js')}}"></script>
+<script src="{{asset('assets/js/owl.carousel.min.js')}}"></script>
+<script src="{{asset('assets/js/jquery.magnific-popup.min.js')}}"></script>
+<!-- Main JS File -->
+<script src="{{asset('assets/js/main.js')}}"></script>
+<script src="{{asset('assets/js/demos/demo-21.js')}}"></script>
 </body>
 
+<!-- molla/index-21.html  22 Nov 2019 10:01:54 GMT -->
 </html>
